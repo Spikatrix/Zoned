1c50da30b2d8e5283fa848093e3c18831da3907d
Spikatrix <12792882+Spikatrix@users.noreply.github.com>
2020-08-18 20:08:38 +0530

    Improved the versionUpdate.sh script

--- versionUpdate.sh
+++ versionUpdate.sh
@@ -5,39 +5,24 @@ read -p "New version name: " newVersionName
 read -p "Are you sure you wanna do this? (Y/N) " proceed
 
 if [ "$proceed" == "Y" ]; then
-	grep -l "$oldVersionName" android/build.gradle | while read filename
-	do
-		sed -i -e "s/$oldVersionName/$newVersionName/g" "$filename"
-	done
+	files=(
+		"android/build.gradle"
+		"desktop/build.gradle"
+		"core/src/com/cg/zoned/Constants.java"
+		"README.md"
+	)
 
-	grep -l "$oldVersionName" desktop/build.gradle | while read filename
-	do
-		sed -i -e "s/$oldVersionName/$newVersionName/g" "$filename"
-	done
-
-	grep -l "$oldVersionName" core/src/com/cg/zoned/Constants.java | while read filename
-	do
-		sed -i -e "s/$oldVersionName/$newVersionName/g" "$filename"
-	done
+	sed -i -e "s/$oldVersionName/$newVersionName/g" "${files[@]}"
 
 	hyphenPos=`echo "$oldVersionName" | grep '-' -oba --color=never | cut -c1`
 	oldHyphenDoubled=`echo "${oldVersionName:0:$hyphenPos}-${oldVersionName:$hyphenPos}"`
 	newHyphenDoubled=`echo "${newVersionName:0:$hyphenPos}-${newVersionName:$hyphenPos}"`
 
-	grep -l "$oldHyphenDoubled" README.md | while read filename
-	do
-		sed -i -e "s/$oldHyphenDoubled/$newHyphenDoubled/g" "$filename"
-	done
+	sed -i -e "s/$oldHyphenDoubled/$newHyphenDoubled/g" "README.md"
 
-	grep -l "$oldVersionName" README.md | while read filename
-	do
-		sed -i -e "s/$oldVersionName/$newVersionName/g" "$filename"
-	done
+	sed -i -e "s/ENABLE_DISCORD_RPC = false/ENABLE_DISCORD_RPC = true/g" "core/src/com/cg/zoned/Constants.java"
 
-	grep -l "ENABLE_DISCORD_RPC = false" core/src/com/cg/zoned/Constants.java | while read filename
-	do
-		sed -i -e "s/ENABLE_DISCORD_RPC = false/ENABLE_DISCORD_RPC = true/g" "$filename"
-	done
+	echo "Done :D"
 else
-	echo "Alright, aborted"
+	echo "Alright, aborted :/"
 fi
