b48da4e72ea2e120af758b112510287e22314e4b
Spikatrix <12792882+Spikatrix@users.noreply.github.com>
2020-12-06 20:44:53 +0530

    Switched back to LWJGL2
    This was done mainly because switching to LWJGL3 increased the final JAR size from 11 MB to 16 MB. Since I don't use LWJGL3 specific features currently, there's no justifiable reason for the increased size in my case which is why I downgraded.

--- desktop/src/com/cg/zoned/desktop/DesktopLauncher.java
+++ desktop/src/com/cg/zoned/desktop/DesktopLauncher.java
@@ -1,17 +1,17 @@
 package com.cg.zoned.desktop;
 
 import com.badlogic.gdx.Files;
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.cg.zoned.Zoned;
 
 public class DesktopLauncher {
     public static void main(String[] args) {
-        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-        config.setTitle("Zoned");
-        config.useVsync(true);
-        config.setBackBufferConfig(8, 8, 8, 8, 16, 0, 4);
-        config.setWindowIcon(Files.FileType.Internal, "icons/ic_zoned_desktop_icon.png");
-        new Lwjgl3Application(new Zoned(new DiscordRPCManager()), config);
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "Zoned";
+        config.vSyncEnabled = true;
+        config.samples = 4;
+        config.addIcon("icons/ic_zoned_desktop_icon.png", Files.FileType.Internal);
+        new LwjglApplication(new Zoned(new DiscordRPCManager()), config);
     }
 }
