4da4e635da461e62738984264193f976c03de948
Spikatrix <12792882+Spikatrix@users.noreply.github.com>
2021-07-16 20:23:24 +0530

    Client pings now gradually change instead of showing the immediate ping

--- core/src/com/cg/zoned/listeners/ClientGameListener.java
+++ core/src/com/cg/zoned/listeners/ClientGameListener.java
@@ -9,16 +9,21 @@ import com.esotericsoftware.kryonet.Listener;
 public class ClientGameListener extends Listener {
     private ClientGameConnectionHandler connectionHandler;
 
+    private final int pingCountLimit = 8;
+    private final int[] previousPings;
+    private int pingListFillCount;
+    private int pingListIndex;
+
     public ClientGameListener(ClientGameConnectionHandler connectionHandler) {
         this.connectionHandler = connectionHandler;
+        this.previousPings = new int[pingCountLimit];
     }
 
     @Override
     public void received(Connection connection, Object object) {
         if (object instanceof BufferDirections) {
             BufferDirections bd = (BufferDirections) object;
-            connection.updateReturnTripTime(); // Updates the ping
-            connectionHandler.clientUpdateDirections(bd, connection.getReturnTripTime());
+            connectionHandler.clientUpdateDirections(bd, updatePing(connection));
         } else if (object instanceof BufferGameEnd) {
             BufferGameEnd bge = (BufferGameEnd) object;
             connectionHandler.clientGameEnd(bge.restartGame);
@@ -26,6 +31,30 @@ public class ClientGameListener extends Listener {
             BufferPlayerLeft bpd = (BufferPlayerLeft) object;
             connectionHandler.clientPlayerDisconnected(bpd.playerName, bpd.disconnected);
         }
+    }
+
+    /**
+     * Updates and records pings
+     *
+     * @param connection The connection with which pings are updated and recorded
+     * @return Average of the last {@link #pingCountLimit} pings
+     */
+    private int updatePing(Connection connection) {
+        connection.updateReturnTripTime();
+        int ping = connection.getReturnTripTime();
+
+        previousPings[pingListIndex] = ping;
+        pingListIndex = (pingListIndex + 1) % pingCountLimit;
+        if (pingListFillCount < pingCountLimit) {
+            pingListFillCount++;
+        }
+
+        int pingSum = 0;
+        for (int i = 0; i < pingListFillCount; i++) {
+            pingSum += previousPings[i];
+        }
+
+        return pingSum / pingListFillCount;
     }
 
     @Override
